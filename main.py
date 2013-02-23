61f7339496fd31526ea76b0da134dd38ce401242
Polytonic <Sniglet.Sesquipedalian@Gmail.com>
2013-02-23 15:06:34 -0500

    Restoring to Default App Engine State

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print "Hello Riverside"
+# Modify Python System Path for Simpler Imports
+import os, sys
+filepath = os.path.dirname(os.path.realpath(__file__))
+#sys.path.append(filepath + '/dulwich')
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
